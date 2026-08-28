1d25acc156f6b46c6cd3e04c29bbf8802ded09d9
Dinosaur203 <priyanshu491rana@gmail.com>
2026-08-28 15:37:48 +0530

    feat: switch LLM to OpenAI gpt-4o-mini

--- X_post_tool/Backend/LLMs.py
+++ X_post_tool/Backend/LLMs.py
@@ -1,14 +1,8 @@
-#from langchain_openai import ChatOpenAI
+import os
 from dotenv import load_dotenv
-#from openai import OpenAI
-from groq import Groq
-from langchain_groq import ChatGroq
-
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-
-import tempfile
-import os
 
 load_dotenv()
 
@@ -26,19 +20,10 @@ prompt = PromptTemplate.from_template(
     """
 )
 
-#model = ChatOpenAI(model="gpt-4o-mini", temperature=0.2)
-
-model = ChatGroq(
-    model="llama-3.1-8b-instant",
-    temperature=0.2,
-    api_key=os.getenv("GROQ_API_KEY")
+model = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0.2
 )
-
-#model = OpenAI(
-#  base_url = "https://integrate.api.nvidia.com/v1",
-#  api_key = "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"
-#)
-
 
 parser = StrOutputParser()
 
